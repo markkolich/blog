f8da2cce7a4b228f3078fc829f6bba5d91085967
Mark Kolich <social@kolich.com>
2013-12-28 15:00:21 -0800

    CSS tweaks

--- src/main/webapp/static/js/kolich.blog.twitter.js
+++ src/main/webapp/static/js/kolich.blog.twitter.js
@@ -12,10 +12,19 @@
 
         sampleJson = {"tweets":[{"id_str":"417006335452913664","created_at":"2013-12-28T10:57:12.000Z","text":"Oh hey, last Saturday of the year."},{"id_str":"416823787485536256","created_at":"2013-12-27T22:51:49.000Z","text":"@killerswan ever."},{"id_str":"416823348748767232","created_at":"2013-12-27T22:50:05.000Z","text":"In Scala, I strongly dislike having to write:\n  val x \u003d if(c) \"foo\" else \"bar\"\n\nOh, idk, how about:\n  val x \u003d c ? \"foo\" : \"bar\"\n\n#ternary"},{"id_str":"416818571738509313","created_at":"2013-12-27T22:31:06.000Z","text":"@royclarkson the worst"},{"id_str":"416818190228783104","created_at":"2013-12-27T22:29:35.000Z","text":"RT @embee: what happens when non-tech people discover web developer tools http://t.co/ApyRlC4niw"},{"id_str":"416759582044585985","created_at":"2013-12-27T18:36:42.000Z","text":"RT @zugzugglug: For those who haven’t yet experienced the joy of programming, now’s a great week to try an Hour of Code: http://t.co/hpoDKJ…"},{"id_str":"416759174249213952","created_at":"2013-12-27T18:35:04.000Z","text":"Fine-Grained Concurrency with Guava \"Striped\" http://t.co/xKwxxTJ0ob #java"},{"id_str":"416676341400875008","created_at":"2013-12-27T13:05:55.000Z","text":"It\u0027ll be quick they say.... two hours later."},{"id_str":"416675952169451520","created_at":"2013-12-27T13:04:23.000Z","text":"@benmilligan +1"},{"id_str":"416674100866596864","created_at":"2013-12-27T12:57:01.000Z","text":"Man sit, waiting, hungry. Woman shop, all day long, there is nothing else. #poetry"}]},
 
-        linkify = function(text) {
-            var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-            return text.replace(exp,"<a href='$1' target='_blank'>$1</a>");
-        },
+        linkify = (function() {
+            var links = function(tweet) {
+                var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+                return tweet.replace(exp,"<a href='$1' target='_blank'>$1</a>");
+            },
+            users = function(tweet) {
+                var exp = /\B\@([\w\-]+)/gim;
+                return tweet.replace(exp, "<a href='https://twitter.com/$1' target='_blank'>@$1</a>");
+            };
+            return function(tweet) {
+                return users(links(tweet));
+            };
+        }()),
 
         // Logic borrowed from http://williamsportwebdeveloper.com/cgi/wp/?p=503
         // Refactored for my own needs.
@@ -50,10 +59,13 @@
             if(tweets && tweets.length > 0) {
                 var ul = $('<ul>');
                 for(var i in tweets) {
-                    var tweet = tweets[i], li = $('<li>');
-                    li.append(linkify(tweet.text));
+                    var tweet = tweets[i], li = $('<li>').addClass('tweet small');
+                    var text = $('<p>').append(linkify(tweet.text));
+                    var timestamp = $('<p>').append(tweet.created_at).addClass('smaller');
+                    li.append(text).append(timestamp);
                     ul.append(li);
                 }
+                ul.find('li.tweet').first().addClass('first');
                 tweetBody.append(ul);
             }
         };
