b5037180b2157415bd80a9229ea4307020af056b
Mark Kolich <social@kolich.com>
2014-03-23 12:36:21 -0700

    Bumping blog to use Curacao 2.0-RC3

--- project/Build.scala
+++ project/Build.scala
@@ -42,8 +42,8 @@ object Dependencies {
 
   // External dependencies
 
-  private val curacao = "com.kolich.curacao" % "curacao" % "2.0-RC2" % "compile"
-  private val curacaoGson = "com.kolich.curacao" % "curacao-gson" % "2.0-RC2" % "compile"
+  private val curacao = "com.kolich.curacao" % "curacao" % "2.0-RC3" % "compile"
+  private val curacaoGson = "com.kolich.curacao" % "curacao-gson" % "2.0-RC3" % "compile"
 
   // Jetty 9 stable, version 9.1.1.v20140108 (as of 2/7/14)
   private val jettyWebApp = "org.eclipse.jetty" % "jetty-webapp" % "9.1.1.v20140108" % "container"
